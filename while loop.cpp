d0631cf004273f9231819215db0e042ea25d7e97
sathvik004 <93913025+sathvik004@users.noreply.github.com>
2021-11-25 09:58:05 +0530

    while loop

---
+++ while loop.cpp
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main()
+{
+	int a,n;
+    a=1;
+	printf("Enter the value");
+	scanf("%d",&n);
+	for(a=1;a<n;a++)
+	{
+		printf("\na after increament%d",a);
+	}
+	return 0;
+}
