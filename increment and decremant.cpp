6bddac1812ed3a13f776e745d5b6ff742171dce7
sathvik004 <93913025+sathvik004@users.noreply.github.com>
2021-11-25 09:55:24 +0530

    increament and decreament

---
+++ increment and decremant.cpp
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()
+{
+	int a=1,b=100;
+	for(a=1;a<11;a++)
+	{
+		printf("\na after increament%d",a);
+	}
+	for(b=-2;b>-10;b--)
+	{
+		printf("\nb after decreament%d",b);
+	}
+	return 0;
+}
