038f36f44837b9c024fe644ec3c6622d2fd307a3
sathvik004 <93913025+sathvik004@users.noreply.github.com>
2021-12-27 15:58:30 +0530

    Add files via upload

---
+++ arthimetic operators witout switch case.cpp
@@ -0,0 +1,12 @@
+#include<stdio.h>
+main()
+{
+	float a,b;
+	printf("Enter the two numbers:");
+	scanf("%f %f",&a,&b);
+	printf("\nsum of two numbers:%f",a+b);
+	printf("\ndifference of two numbers:%f",a-b);
+	printf("\nmultilpication of two numbers:%f",a*b);
+	printf("\ndivision of two numbers:%f",a/b);
+	return 0;
+}
